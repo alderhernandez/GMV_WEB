292c9d1ee6fcff8d8cf9bb264ea37b5ab861617f
ALDER HERNANDEZ <analista.guma@gmail.com>
2017-06-15 08:40:09 -0600

    15-06-2017

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,14 +1,14 @@
 
-<div class="col center s12 l5">
+<div class="col center s12 l5"><br><br>
     <form class="form" method="post" action="<?php echo base_url('index.php/login')?>">
-        <div class="row login-logo center" style="width: 230px!important; height:230px;">
+        <div class="row login-logo center" style="width: 200px!important; height:200px;">
             <img  src="<?PHP echo base_url();?>assets/img/gmv_icono.png" style="width:100%;height:100%;">
-        </div>
+        </div><br>
         <div  class="row">
             <div class="col s10 m10 l6 offset-l3 offset-m1 offset-s1">  
                 <input style="width: 60%;"  placeholder="USUARIO" name="txtUsuario" id="nombre" type="text" class=" validate">
             </div>
-        </div>
+        </div><br>
         <div class="row">
             <div class="col s10 m10 l6 offset-l3 offset-m1 offset-s1">
                 <input style="width: 60%;" placeholder="CONTRASEÑA" name="txtpassword" id="pass" type="password" class="validate">
